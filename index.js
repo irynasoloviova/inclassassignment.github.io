d457b94f0e35ea0e33cc354f2dfcb28f95b79402
irynasoloviova <71346614+irynasoloviova@users.noreply.github.com>
2020-09-16 22:20:10 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+function multiplyBy(){
+	a = document.getElementById("first").value;
+    b = document.getElementById("second").value;
+        document.getElementById("result").innerHTML = a * b;
+}
+
+function divideBy(){ 
+	a = document.getElementById("first").value;
+    b = document.getElementById("second").value;
+        document.getElementById("result").innerHTML = a / b;
+}
+
+function add(){ 
+	a = document.getElementById("first").value;
+    b = document.getElementById("second").value;
+        document.getElementById("result").innerHTML = a + b;
+}
+
+function minus(){ 
+	a = document.getElementById("first").value;
+    b = document.getElementById("second").value;
+        document.getElementById("result").innerHTML = a - b;
+}
